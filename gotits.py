dd7e04c21ae40c681d4e5e36d6281cde5c0bc67d
Huzaifahi7 <49676339+Huzaifahi7@users.noreply.github.com>
2022-01-20 17:32:45 +0500

    Update gotits.py

--- gotits.py
+++ gotits.py
@@ -5,7 +5,7 @@ import serial
 
 
 # Define a function for the thread
-def serial_monitor():
+def serial_monitor(iss):
     global line
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     ser.reset_input_buffer()
@@ -13,14 +13,14 @@ def serial_monitor():
         if ser.in_waiting > 0:
             line = ser.readline().decode('ascii').rstrip()
 
-def motor_control():
+def motor_control(ssi):
     while True:
         print(line)
         time.sleep(1)
 line=0
 try:
-    _thread.start_new_thread(serial_monitor)
-    _thread.start_new_thread(motor_control)
+    _thread.start_new_thread(serial_monitor,(1,))
+    _thread.start_new_thread(motor_control,(1,))
 except:
     print ("Error: unable to start thread")
 
